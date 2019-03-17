7aa762ac86f0f11734aaf920aebba00a05ce4585
Aaron Weimann <aaron.weimann@uni-duesseldorf.de>
2019-03-17 13:18:39 +0100

    Added misclassified phylogeny analysis

---
+++ misclassified_phylogeny/graphlan.sh
@@ -0,0 +1,4 @@
+graphlan.py tree_annot_Tobra.xml tree_tobra.png
+graphlan.py tree_annot_Cipro.xml tree_cipro.png
+graphlan.py tree_annot_Mero.xml tree_mero.png
+graphlan.py tree_annot_Cefta.xml tree_cefta.png
---
+++ misclassified_phylogeny/graphlan_annot.sh
@@ -0,0 +1,5 @@
+graphlan_annotate.py --annot annot_graphlan_Tobra+gpa_expr-miscl.txt mi-tip.414_pruned.txt tree_annot_Tobra.xml
+graphlan_annotate.py --annot annot_graphlan_Cefta+gpa_expr-miscl.txt mi-tip.414_pruned.txt tree_annot_Cefta.xml
+graphlan_annotate.py --annot annot_graphlan_Mero+gpa_expr-miscl.txt mi-tip.414_pruned.txt tree_annot_Mero.xml
+graphlan_annotate.py --annot annot_graphlan_Cipro+snp-miscl.txt mi-tip.414_pruned.txt tree_annot_Cipro.xml
+
